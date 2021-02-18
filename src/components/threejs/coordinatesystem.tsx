64b7116f9db5daf84d770d9f963843a9a661b792
Daniel Toplak <homer.simpson123@gmx.de>
2021-02-18 01:15:12 +0100

    refactor: cleanup

--- src/components/threejs/coordinatesystem.tsx
+++ src/components/threejs/coordinatesystem.tsx
@@ -1,20 +1,6 @@
 import React from 'react';
+import { Vector3 } from 'three';
 import { Arrow } from './arrow';
-import * as THREE from 'three';
-
-function randomVecs(count: number): THREE.Vector3[] {
-  const array: THREE.Vector3[] = [];
-  for (let i = 0; i < count; i++) {
-    const rnd = new THREE.Vector3(
-      THREE.MathUtils.randFloat(-1, 1),
-      THREE.MathUtils.randFloat(-1, 1),
-      THREE.MathUtils.randFloat(-1, 1)
-    );
-
-    array.push(rnd);
-  }
-  return array;
-}
 
 type CoordinateSystemProps = {
   size: number;
@@ -27,23 +13,14 @@ const defaultProps = {
 };
 
 export const CoordinateSystem = (props: CoordinateSystemProps): React.ReactElement => {
-  const vecs = randomVecs(50);
-
   const size = props.size / 2;
   return (
     <>
-      <Arrow start={new THREE.Vector3(0, 0, 0)} end={new THREE.Vector3(size, 0, 0)} color={0xff0000} />
-      <Arrow start={new THREE.Vector3(0, 0, 0)} end={new THREE.Vector3(0, size, 0)} color={0x00ff00} />
-      <Arrow start={new THREE.Vector3(0, 0, 0)} end={new THREE.Vector3(0, 0, size)} color={0x0000ff} />
+      <Arrow start={new Vector3(0, 0, 0)} end={new Vector3(size, 0, 0)} color={0xff0000} />
+      <Arrow start={new Vector3(0, 0, 0)} end={new Vector3(0, size, 0)} color={0x00ff00} />
+      <Arrow start={new Vector3(0, 0, 0)} end={new Vector3(0, 0, size)} color={0x0000ff} />
 
       {props.showGrid && <gridHelper args={[props.size, props.size * 2]}></gridHelper>}
-
-      <Arrow start={new THREE.Vector3(0, 0, 0)} end={new THREE.Vector3(2, 1, -1)} color={0xff00ff} />
-      {/* <Arrow start={new THREE.Vector3(0, 0, 0)} end={rnd} color={0xffff00} /> */}
-
-      {/* {vecs.map((v, i) => {
-        return <Arrow key={i} start={new THREE.Vector3(0, 0, 0)} end={v} color={0xffff00} />;
-      })} */}
     </>
   );
 };
