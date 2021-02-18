ee63b1ff1eb9b00fd0d01b642f15012e91d03ba1
Daniel Toplak <homer.simpson123@gmx.de>
2021-02-18 01:14:47 +0100

    fix: animations

--- src/components/threejs/arrow.tsx
+++ src/components/threejs/arrow.tsx
@@ -1,49 +1,56 @@
-import React from 'react';
 import { Line } from '@react-three/drei';
-import { Color } from 'react-three-fiber';
-import * as THREE from 'three';
+import React, { useRef } from 'react';
 import { a, useSpring } from 'react-spring/three';
-import { easeOutElastic, easeOutBounce, easeOutExpo } from '../../utils/easings';
+import { Color } from 'react-three-fiber';
+import { Euler, Quaternion, Vector3 } from 'three';
+import { Line2 } from 'three/examples/jsm/lines/Line2';
+import { easeOutBounce, easeOutExpo } from '../../utils/easings';
 
 interface ArrowProps {
-  start: THREE.Vector3;
-  end: THREE.Vector3;
+  start: Vector3;
+  end: Vector3;
   color: Color;
 }
 
 export const Arrow = (props: ArrowProps): React.ReactElement => {
+  const { start, end, color } = props;
+
   const tipRadius = 0.04;
   const tipLength = 0.075;
-  const up = new THREE.Vector3(0, 1, 0);
+  const up = new Vector3(0, 1, 0);
 
-  const direction = new THREE.Vector3();
-  direction.subVectors(props.end, props.start).normalize();
+  const direction = new Vector3();
+  direction.subVectors(end, start).normalize();
 
-  const rot = new THREE.Quaternion();
+  const rot = new Quaternion();
   rot.setFromUnitVectors(up, direction);
-  const euler = new THREE.Euler();
+  const euler = new Euler();
   euler.setFromQuaternion(rot);
 
   const rotationVector = euler.toVector3();
 
-  const startPoint = props.start.toArray();
-  const endPoint = props.end.toArray();
+  const startPoint = start.toArray();
+  const endPoint = end.toArray();
   const rotation = rotationVector.toArray();
+
+  const lineRef = useRef<Line2>(null);
 
   const arrowLineAnimation = useSpring({
     from: {
-      scale: [0, 0, 0],
-      tipPos: props.start.toArray(),
+      endPoint: startPoint,
     },
     to: {
-      scale: [1, 1, 1],
-      tipPos: props.end.toArray(),
+      endPoint: endPoint,
     },
     config: {
       duration: 2000,
       easing: easeOutBounce,
     },
     delay: 2000,
+    onFrame: (value) => {
+      const positions = [...startPoint, ...value.endPoint];
+      lineRef.current.geometry.setPositions(positions);
+    },
   });
 
   const tipVisibilityAnimation = useSpring({
@@ -62,16 +69,14 @@ export const Arrow = (props: ArrowProps): React.ReactElement => {
 
   return (
     <group>
-      <a.group scale={arrowLineAnimation.scale}>
-        <Line points={[startPoint, endPoint]} color={props.color} lineWidth={3} />
-      </a.group>
-      <a.group position={arrowLineAnimation.tipPos} rotation={rotation}>
+      <Line ref={lineRef} points={[startPoint, startPoint]} color={color} lineWidth={3} />
+
+      <a.group position={arrowLineAnimation.endPoint} rotation={rotation}>
         <mesh>
           <coneBufferGeometry attach="geometry" args={[tipRadius, tipLength]} />
-          {/* <meshBasicMaterial attach="material" color={props.color} /> */}
           <a.meshStandardMaterial
             attach="material"
-            color={props.color}
+            color={color}
             opacity={tipVisibilityAnimation.opacity}
             transparent={true}
           />
