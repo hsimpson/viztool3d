f0a4ce9fe408fe89d2c40fe42ba219d4955f2789
Daniel Toplak <homer.simpson123@gmx.de>
2021-03-03 23:34:37 +0100

    feat: parallel arrows for directional light

--- src/components/threejs/lights/directional.tsx
+++ src/components/threejs/lights/directional.tsx
@@ -1,6 +1,6 @@
-import React from 'react';
-import { Color, Object3D, Vector3 } from 'three';
-import { Arrow } from '../arrow';
+import React, { useEffect, useRef } from 'react';
+import { Color, Group, Object3D, Vector3 } from 'three';
+import { Arrow, ArrowT } from '../arrow';
 import { Plane } from '../plane';
 
 interface DirectionalLightProps {
@@ -11,21 +11,45 @@ interface DirectionalLightProps {
 export const DirectionalLight = (props: DirectionalLightProps): React.ReactElement => {
   const { position, target } = props;
 
-  const arrowStartPositions: Vector3[] = [position];
+  const direction = new Vector3();
+  direction.subVectors(target, position);
+  const length = direction.length();
 
-  const color = new Color('#ebe70c');
+  const arrows: ArrowT[] = [];
+
+  const count = 4;
+  const step = 0.2;
+  const begin = (count * step) / 2 - 0.5 * step;
+  for (let h = 0; h < count; h++) {
+    for (let w = 0; w < count; w++) {
+      const x = w * step;
+      const y = h * step;
+      const start = new Vector3(x - begin, y - begin, 0);
+      const end = new Vector3(x - begin, y - begin, length);
+
+      arrows.push({
+        start,
+        end,
+      });
+    }
+  }
+
   const lightTarget = new Object3D();
   lightTarget.position.copy(target);
 
-  const direction = new Vector3();
-  direction.subVectors(target, position);
+  const color = new Color('#ebe70c');
+
+  const groupRef = useRef<Group>();
+
+  useEffect(() => {
+    groupRef.current.lookAt(direction);
+  });
 
   return (
-    <group>
+    <group ref={groupRef} position={position}>
       <directionalLight target={lightTarget} />
-      <Plane color={color} position={position} lookAt={direction} />
-      {arrowStartPositions.map((start, index) => {
-        return <Arrow key={index} start={start} end={target} color={color} />;
+      {arrows.map((arrow, index) => {
+        return <Arrow key={index} start={arrow.start} end={arrow.end} color={color} opacity={0.3} />;
       })}
     </group>
   );
