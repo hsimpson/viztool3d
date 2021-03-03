cd2786720e808d1623ff66e34fde048fd20fa956
Daniel Toplak <homer.simpson123@gmx.de>
2021-03-03 23:33:33 +0100

    feat: line plane and opacity

--- src/components/threejs/plane.tsx
+++ src/components/threejs/plane.tsx
@@ -1,18 +1,22 @@
 import React, { useRef, useEffect } from 'react';
 import { Color } from 'react-three-fiber';
 import { Vector3, Mesh, DoubleSide } from 'three';
+import { Line } from '@react-three/drei';
 
 interface PlaneProps {
   width?: number;
   height?: number;
   color: Color;
+  opacity?: number;
   position?: Vector3;
   lookAt?: Vector3;
+  line?: boolean;
 }
 
 // creates a plane with normal pointing to (1,0,0)
 export const Plane = (props: PlaneProps): React.ReactElement => {
-  const { width, height, color, position, lookAt } = props;
+  let { width, height } = props;
+  const { color, opacity, position, lookAt, line } = props;
   const meshRef = useRef<Mesh>();
 
   useEffect(() => {
@@ -21,10 +25,38 @@ export const Plane = (props: PlaneProps): React.ReactElement => {
     }
   }, [lookAt]);
 
-  return (
-    <mesh ref={meshRef} position={position}>
-      <planeBufferGeometry attach="geometry" args={[width, height]} />
-      <meshStandardMaterial attach="material" color={color} side={DoubleSide} />
-    </mesh>
-  );
+  const getPlaneMesh = (): React.ReactElement => {
+    if (line) {
+      const points: Vector3[] = [];
+      width = width ?? 1;
+      height = height ?? 1;
+
+      points.push(new Vector3(-width / 2, height / 2, 0));
+      points.push(new Vector3(width / 2, height / 2, 0));
+      points.push(new Vector3(width / 2, -height / 2, 0));
+      points.push(new Vector3(-width / 2, -height / 2, 0));
+      points.push(new Vector3(-width / 2, height / 2, 0));
+
+      return (
+        <mesh ref={meshRef} position={position}>
+          <Line points={points} lineWidth={2} color={color} opacity={opacity ?? 1} transparent={true} />
+        </mesh>
+      );
+    } else {
+      return (
+        <mesh ref={meshRef} position={position}>
+          <planeBufferGeometry attach="geometry" args={[width, height]} />
+          <meshStandardMaterial
+            attach="material"
+            color={color}
+            side={DoubleSide}
+            opacity={opacity ?? 1}
+            transparent={true}
+          />
+        </mesh>
+      );
+    }
+  };
+
+  return getPlaneMesh();
 };
