3c4f0dc4a6d60a8395480605fa8ea58678bd1e84
Daniel Toplak <homer.simpson123@gmx.de>
2021-03-17 21:05:04 +0100

    fix: sphere segments

--- src/components/threejs/primitives/sphere.tsx
+++ src/components/threejs/primitives/sphere.tsx
@@ -9,9 +9,13 @@ interface SphereProps {
 
 export const Sphere = (props: SphereProps): React.ReactElement => {
   const { color, radius, opacity } = props;
+
+  const widthSegments = 32;
+  const heightSegments = 32;
+
   return (
     <mesh>
-      <sphereBufferGeometry attach="geometry" args={[radius, 32, 32]} />
+      <sphereBufferGeometry attach="geometry" args={[radius, widthSegments, heightSegments]} />
       <meshStandardMaterial attach="material" color={color} transparent={true} opacity={opacity ?? 1} />
     </mesh>
   );
