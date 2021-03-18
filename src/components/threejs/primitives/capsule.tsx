c708084c71aa3083dd3284d652156037f342ecf8
Daniel Toplak <homer.simpson123@gmx.de>
2021-03-18 19:55:14 +0100

    feat: add capsule 3d component

---
+++ src/components/threejs/primitives/capsule.tsx
@@ -0,0 +1,34 @@
+import React, { useMemo } from 'react';
+import { Color } from 'react-three-fiber';
+import { MeshStandardMaterial } from 'three';
+
+interface CapsuleProps {
+  color: Color;
+  opacity?: number;
+  radiusTop: number;
+  radiusBottom: number;
+  height: number;
+}
+
+export const Capsule = ({ opacity = 1.0, ...props }: CapsuleProps): React.ReactElement => {
+  const { color, radiusTop, radiusBottom, height } = props;
+
+  const material = useMemo(() => new MeshStandardMaterial({ color, opacity, transparent: true }), [color, opacity]);
+
+  const radialSegments = 32;
+  const heightSegments = 8;
+
+  return (
+    <>
+      <mesh material={material} position={[0, height / 2, 0]}>
+        <sphereBufferGeometry args={[radiusTop, radialSegments, radialSegments, 0, Math.PI * 2, 0, Math.PI / 2]} />
+      </mesh>
+      <mesh material={material}>
+        <cylinderBufferGeometry args={[radiusTop, radiusBottom, height, radialSegments, heightSegments, true]} />
+      </mesh>
+      <mesh material={material} position={[0, -height / 2, 0]} rotation-x={Math.PI}>
+        <sphereBufferGeometry args={[radiusBottom, radialSegments, radialSegments, 0, Math.PI * 2, 0, Math.PI / 2]} />
+      </mesh>
+    </>
+  );
+};
