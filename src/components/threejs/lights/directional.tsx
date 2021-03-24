a42788a86f8222d1fbe417d373feb2de5135e7bb
Daniel Toplak <homer.simpson123@gmx.de>
2021-03-24 22:24:11 +0100

    feat: improve directional light

--- src/components/threejs/lights/directional.tsx
+++ src/components/threejs/lights/directional.tsx
@@ -1,7 +1,8 @@
 import React, { useEffect, useRef } from 'react';
-import { Color, Group, Object3D, Vector3 } from 'three';
+import { Color, Group, MathUtils, Object3D, Vector3 } from 'three';
+import { getEulerRotationFromVectors } from '../../../utils/math';
 import { Arrow, ArrowT } from '../arrow';
-// import { Plane } from '../primitives/plane';
+import { Capsule } from '../primitives/capsule';
 import { Sphere } from '../primitives/sphere';
 
 interface DirectionalLightProps {
@@ -9,23 +10,19 @@ interface DirectionalLightProps {
   target: Vector3;
 }
 
-export const DirectionalLight = (props: DirectionalLightProps): React.ReactElement => {
-  const { position, target } = props;
-
-  const direction = new Vector3();
-  direction.subVectors(target, position);
-  const length = direction.length();
-
+const getParallelArrows = (radius: number, length: number): ArrowT[] => {
   const arrows: ArrowT[] = [];
 
   const count = 4;
   const step = 0.2;
+  const startOffset = 1.8;
+
   const begin = (count * step) / 2 - 0.5 * step;
   for (let h = 0; h < count; h++) {
     for (let w = 0; w < count; w++) {
       const x = w * step;
       const y = h * step;
-      const start = new Vector3(x - begin, y - begin, 0);
+      const start = new Vector3(x - begin, y - begin, radius * startOffset);
       const end = new Vector3(x - begin, y - begin, length);
 
       arrows.push({
@@ -34,12 +31,69 @@ export const DirectionalLight = (props: DirectionalLightProps): React.ReactEleme
       });
     }
   }
+  return arrows;
+};
+
+const getSphericalRays = (radius: number): ArrowT[] => {
+  const directions: ArrowT[] = [];
+  const stepAngle = 45;
+  const count = 360 / stepAngle;
+  const startOffset = 1.8;
+  const lengthFactor = 0.5;
+
+  // optimization: start at 1 to not put the poles into the array often
+  for (let i = 1; i < count; i++) {
+    for (let j = 1; j < count; j++) {
+      // let phi = 0;
+      let phi = i * stepAngle;
+      let theta = j * stepAngle;
+      // console.log(`𝜑 = ${phi}, 𝜃 = ${theta}`);
+
+      phi = MathUtils.degToRad(phi);
+      theta = MathUtils.degToRad(theta);
+
+      const x = Math.sin(theta) * Math.sin(phi);
+      const y = Math.cos(theta);
+      const z = Math.sin(theta) * Math.cos(phi);
+      // console.log(`${x}, ${y}, ${z}`);
+      const direction = new Vector3(x, y, z);
+      const start = direction.clone().multiplyScalar(radius * startOffset);
+      const end = start.clone().add(start.clone().setLength(radius * lengthFactor));
+      directions.push({
+        start,
+        end,
+      });
+    }
+  }
+
+  // adding the poles
+  const northDirection = new Vector3(0, 1, 0);
+  const northStart = northDirection.clone().multiplyScalar(radius * startOffset);
+  const northEnd = northStart.clone().add(northStart.clone().setLength(radius * lengthFactor));
+
+  const southDirection = new Vector3(0, -1, 0);
+  const southStart = southDirection.clone().multiplyScalar(radius * startOffset);
+  const southEnd = southStart.clone().add(southStart.clone().setLength(radius * lengthFactor));
+  directions.push({
+    start: northStart,
+    end: northEnd,
+  });
+  directions.push({
+    start: southStart,
+    end: southEnd,
+  });
+
+  return directions;
+};
+
+export const DirectionalLight = (props: DirectionalLightProps): React.ReactElement => {
+  const { position, target } = props;
+
+  const direction = new Vector3();
+  direction.subVectors(target, position);
 
   const lightTarget = new Object3D();
   lightTarget.position.copy(target);
-
-  const color = new Color('#ebe70c');
-  const opacity = 0.5;
 
   const groupRef = useRef<Group>();
 
@@ -47,12 +101,37 @@ export const DirectionalLight = (props: DirectionalLightProps): React.ReactEleme
     groupRef.current.lookAt(direction);
   });
 
+  const radius = 0.2;
+  const color = new Color('#ebe70c');
+  const opacity = 1.0;
+
+  const parallelArrows = getParallelArrows(radius, direction.length());
+  const sphericalRays = getSphericalRays(radius);
+
   return (
-    <group ref={groupRef} position={position}>
-      <directionalLight target={lightTarget} />
-      <Sphere color={color} radius={0.1} opacity={opacity} />
-      {arrows.map((arrow, index) => {
-        return <Arrow key={index} start={arrow.start} end={arrow.end} color={color} opacity={opacity} />;
+    <group position={position}>
+      <group ref={groupRef}>
+        <directionalLight target={lightTarget} />
+        <Sphere color={color} radius={radius} opacity={opacity} />
+        {parallelArrows.map((arrow, index) => {
+          return <Arrow key={index} start={arrow.start} end={arrow.end} color={color} opacity={1.0} />;
+        })}
+      </group>
+      {sphericalRays.map((ray, index) => {
+        const direction = new Vector3();
+        direction.subVectors(ray.end, ray.start);
+        const rotation = getEulerRotationFromVectors(new Vector3(0, 1, 0), direction);
+        return (
+          <group key={index} position={ray.start} rotation={rotation}>
+            <Capsule
+              color={color}
+              opacity={opacity}
+              radiusBottom={radius * 0.15}
+              radiusTop={radius * 0.15}
+              height={direction.length()}
+            />
+          </group>
+        );
       })}
     </group>
   );
