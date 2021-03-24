75b86740e9c7f1d2dd36090433f42e8a99ee9ba0
Daniel Toplak <homer.simpson123@gmx.de>
2021-03-24 22:22:43 +0100

    fix: cleanup

--- src/components/threejs/scene.tsx
+++ src/components/threejs/scene.tsx
@@ -1,25 +1,9 @@
 import { OrbitControls } from '@react-three/drei';
 import React from 'react';
-// import { Cube } from './cube';
-import { CoordinateSystem } from './coordinatesystem';
-import { SkyBox } from './skybox';
-import { DirectionalLight } from './lights/directional';
 import { Vector3 } from 'three';
-import { Arrow } from './arrow';
-
-// function randomVecs(count: number): Vector3[] {
-//   const array: Vector3[] = [];
-//   for (let i = 0; i < count; i++) {
-//     const rnd = new Vector3(
-//       MathUtils.randFloat(-1, 1),
-//       MathUtils.randFloat(-1, 1),
-//       MathUtils.randFloat(-1, 1)
-//     );
-
-//     array.push(rnd);
-//   }
-//   return array;
-// }
+import { CoordinateSystem } from './coordinatesystem';
+import { DirectionalLight } from './lights/directional';
+import { SkyBox } from './skybox';
 
 export const Scene = (): React.ReactElement => {
   const size = 10;
@@ -31,12 +15,6 @@ export const Scene = (): React.ReactElement => {
       <pointLight position={[10, 10, 10]} />
       <CoordinateSystem size={size} showGrid={true} />
       <DirectionalLight position={new Vector3(-1, 1, 0)} target={new Vector3(0, 0, 0)} />
-      {/* <Arrow start={new Vector3(-2, 2, -2)} end={new Vector3(0, 0, 0)} color={0xff00ff} /> */}
-      {/* {vecs.map((v, i) => {
-        return <Arrow key={i} start={new Vector3(0, 0, 0)} end={v} color={0xffff00} />;
-      })} */}
-
-      {/* <Cube /> */}
     </>
   );
 };
